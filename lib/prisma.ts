63985fc51a1191f9b53a2cd202d31bb387bbdb9e
sentianurisnaeni <sentia.nur24@smk.belajar.id>
2026-08-09 14:03:37 +0700

    fix prisma and postcss

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -3,10 +3,6 @@ import { PrismaPg } from "@prisma/adapter-pg";
 
 const connectionString = process.env.DATABASE_URL;
 
-if (!connectionString) {
-  throw new Error("DATABASE_URL belum tersedia");
-}
-
 const adapter = new PrismaPg({
   connectionString,
 });
