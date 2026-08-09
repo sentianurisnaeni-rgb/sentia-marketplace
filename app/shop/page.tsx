991cf00614f83db083cb10752dcd1f9ae1cd8a6f
sentianurisnaeni <sentia.nur24@smk.belajar.id>
2026-08-09 13:41:27 +0700

    fix prisma for vercel

--- app/shop/page.tsx
+++ app/shop/page.tsx
@@ -1,4 +1,4 @@
-import { prisma } from "../lib/prisma";
+import { prisma } from "@/lib/prisma";
 import CartButton from "../components/CartButton";
 
 export default async function ShopPage() {
--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,6 +1,11 @@
+import { PrismaClient } from "../app/generated/prisma/client";
 import { PrismaPg } from "@prisma/adapter-pg";
-import { PrismaClient } from "@prisma/client";
-const connectionString = process.env.DATABASE_URL!;
+
+const connectionString = process.env.DATABASE_URL;
+
+if (!connectionString) {
+  throw new Error("DATABASE_URL belum tersedia");
+}
 
 const adapter = new PrismaPg({
   connectionString,
